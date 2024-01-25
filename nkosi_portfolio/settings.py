87976024cb03ee5983e6567f44f13854c4bba104
nncube023b <nncube023b@student.wethinkcode.co.za>
2024-01-25 09:30:51 +0200

    whitenoise

--- nkosi_portfolio/settings.py
+++ nkosi_portfolio/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -129,7 +130,7 @@ STATICFILES_DIRS = [
 
 # Define the absolute path to the directory where your Django application will serve your static files from
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATTICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
